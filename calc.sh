cfa4b77adf32731cd0e32c734c12f0d2d9d6eabf
danh55162 <danhcoder23072004@gmail.com>
2024-03-25 09:30:37 +0700

    complete basic calc.sh

--- calc.sh
+++ calc.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-read -r a
-read -r operator
-read -r b
+readInput() {
+    read -r input
+    read -r -a arr <<< "$input"
+}
 
-echo 'The expression is: ' "$a" "$operator" "$b"
+readInput
+
+a="${arr[0]}"
+op="${arr[1]}"
+b="${arr[2]}"
+
+echo 'The expression is: '"$a" "$op" "$b"
+
+
+case $op in
+    '+')
+        result=$((a + b))
+        ;;
+    '-')
+        result=$((a - b))
+        ;;
+    'x')
+        result=$((a * b))
+        ;;
+    '/')
+        result=$((a / b))
+        ;;
+    '%')
+        result=$((a % b))
+        ;;
+esac
+
+echo "The result is: $result"
