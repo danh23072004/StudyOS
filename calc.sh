8cf0cf02656b3303ff459f5ff326a9de7b4d0522
danh55162 <danhcoder23072004@gmail.com>
2024-03-25 17:32:13 +0700

    building HISTORY and SYNTAX ERROR feature

--- calc.sh
+++ calc.sh
@@ -1,22 +1,73 @@
 #!/bin/bash
 
+# -- ANS --
+
+ifANS() {
+    if [ "$a" == "ANS" ]; then
+    {
+        a=$result
+    }
+    fi
+    
+    if [ "$b" == "ANS" ]; then
+    {
+        b=$result
+    }
+    fi
+}
+
+# -- HIST --
+
+ifHIST() {
+    echo "$hist"
+}
+
+showHIST() {
+    echo '' 
+}
+
+histCountLine=0
+hist=''
+appendHIST() {
+    if [ $histCountLine -le 5 ]; then
+    {
+        hist="$hist""$input"'\n'
+        ((var+=1))
+    }
+    else
+    {
+        hist="$hist""$input"'\n'
+    }
+    fi
+
+}
+
+# -- READ --
+
 readInput() {
     echo -n '>> '
     read -r input
     read -r -a arr <<< "$input"
 
+    # append history command to variable "hist"
+    appendHIST
+
     a="${arr[0]}"
     op="${arr[1]}"
     b="${arr[2]}"
+
+    ifANS
 }
+
+# -- CALCULATE --
 
 calculate(){
     case $op in
     '+')
-        result=$((a + b))
+        result=$(echo "$a + $b" | bc -l)
         ;;
     '-')
-        result=$((a - b))
+        result=$(echo "$a - $b" | bc -l)
         ;;
     'x')
         result=$(echo "$a * $b" | bc -l)
@@ -25,7 +76,7 @@ calculate(){
         result=$(echo "$a / $b" | bc -l)
         ;;
     '%')
-        result=$((a % b))
+        result=$(echo "$a % $b" | bc -l)
         ;;
     esac
 }
@@ -37,15 +88,11 @@ showOutput(){
     read -n 1 -s -r
 }
 
-showHIST(){
-    echo 'HIST'
-}
-
 # Main program here
 
 readInput
 
-while [[ $a != "EXIT" ]]
+while [[ $input != "EXIT" ]]
 do
     calculate
     showOutput
